4df5829def01b910a2f6dd85601690c7b829dca5
Caleb Bush <calebmatthewbush@gmail.com>
2023-02-09 16:13:52 -0600

    Change component layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Header from './components/Header';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer'
+import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
+      <main>
+        <About />
+        <Projects />
+      </main>
+      <Footer />
     </div>
   );
 }
