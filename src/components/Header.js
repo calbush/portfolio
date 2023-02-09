389ee66e9c177d4ccb038f9d9585a95844e0f1ac
Caleb Bush <calebmatthewbush@gmail.com>
2023-02-09 00:03:27 -0600

    Initial setup

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return(
+        <nav className='header'>
+            <button className='about'>About Me</button>
+            <button className='projects'>Projects</button>
+            <button className='contact'>Contact</button>
+        </nav>
+    )
+}
+
+export default Header
