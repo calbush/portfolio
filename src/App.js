5c6b5a0966a6d1fcc4f4ddd975c659b706ea03fc
Caleb Bush <calebmatthewbush@gmail.com>
2023-02-10 23:45:29 -0600

    Add Intro component and move About component to end of App

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Header from './components/Header';
 import Footer from './components/Footer'
 import About from './components/About';
 import Projects from './components/Projects';
+import Intro from './components/Intro';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <main>
-        <About />
+        <Intro />
         <Projects />
+        <About />
       </main>
       <Footer />
     </div>
