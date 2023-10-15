f8d9845f87e512eb3093316db87733c6f33775a7
Caleb Bush <calebmatthewbush@gmail.com>
2023-10-15 14:27:47 -0500

    Make card an anchor tag

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 const Card = ({ project }) => {
 
     return(
-        <div className='card'>
-            <img className='card-image' src={`${project.image}`} alt='' />
-            <div className='card-text'>
-                <h2>{`${project.title}`}</h2>
-                <p className='card-description'>{`${project.description}`}</p>
+        <a href={project.link} target="_blank" rel="noreferrer" className='no-underline-link'>
+            <div className='card'>
+                <img className='card-image' src={`${project.image}`} alt='' />
+                <div className='card-text'>
+                    <h2>{`${project.title}`}</h2>
+                    <p className='card-description'>{`${project.description}`}</p>
+                </div>
             </div>
-        </div>
+        </a>
     )
 }
 
