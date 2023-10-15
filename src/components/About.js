56f2b07811ef9ebde8c1e0e05b12b580a2a9f954
Caleb Bush <calebmatthewbush@gmail.com>
2023-10-15 14:27:03 -0500

    Fill in text content

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,13 @@ const About = () => {
 
 
     return(
-        <div className='about-container section-container'>
+        <div id='about' className='about-container section-container'>
             <h1 className='about-text-heading'>About Me</h1>
             <div className='about-text-container'>
-                <p className='about-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <p className='about-text'>Hey there. I'm Caleb. I've got a passion for film & tv, photography, video games, and web development. I've also recently taken up bass guitar which has been a blast.</p>
+                <p className='about-text'>I graduated with a B.A. in filmmaking from Columbia College Chicago in the Fall of 2021. After about a year of trying to find consistent work in the film industry, I realized that it wasn't the right career for me, but I still kept my passion for movies!</p>
+                <p className='about-text'>I wanted to find a career where I could frequently learn on the job, utilize my problem solving skills, and still flex my creative muscles. This led me to programming.
+                 I've been teaching myself for about a year and a half. I started with a very basic introductory programming course called <a href='https://www.py4e.com/' rel='noreferrer' target='_blank'>Python For Everybody</a>, where I got a broad overview of python and general programming fundamentals. Eventually, I discovered an open source, full stack, asynchronous web development course called <a href='https://www.theodinproject.com/' rel='noreferrer' target='_blank'>The Odin Project.</a> Some of the technologies and languages I've learned are: Python, Flask, HTML, CSS, Javascript, Node.js, React, Express, MongoDB, and Git. </p>
             </div>
         </div>
     )
