28dec5ddd353981e3ebcaeb65f200170383f1d97
Caleb Bush <calebmatthewbush@gmail.com>
2023-10-15 14:44:05 -0500

    Add link icon to make it more apparent that card is a link for mobile

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,5 @@
+import open from '../assets/images/open-link.png'
+
 const Card = ({ project }) => {
 
     return(
@@ -7,6 +9,9 @@ const Card = ({ project }) => {
                 <div className='card-text'>
                     <h2>{`${project.title}`}</h2>
                     <p className='card-description'>{`${project.description}`}</p>
+                </div>
+                <div className='icons'>
+                    <a href={project.link} target='_blank' rel='noreferrer'><img alt='open-link icon' src={open}/></a>
                 </div>
             </div>
         </a>
