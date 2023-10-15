fe7dadeeb18cc929651df5cbb6f5a183c0a18db0
Caleb Bush <calebmatthewbush@gmail.com>
2023-10-15 14:28:47 -0500

    Update footer structure

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,8 @@ const Footer = () => {
 
     return(
         <footer>
-            <p>Placeholder text</p>
             <div className='logo-holder'>
-                <a href='https://www.instagram.com/calebtothebush/' target='_blank' rel='noreferrer noopener'><img className='insta-logo'src={instalogo} alt='Link to my instagram profile'/></a>
+                <a href='https://www.instagram.com/calebtothebush/' target='_blank' rel='noreferrer noopener'><img className='insta-logo'src={instalogo} alt='open'/></a>
                 <a href='https://github.com/calbush' target='_blank' rel='noreferrer noopener'><img className='github-logo'src={githublogo} alt='Link to my github profile'/></a>
             </div>
         </footer>
