a8ea4672b3857738b40aa3f172cc6ce9391bdf8f
Caleb Bush <calebmatthewbush@gmail.com>
2023-10-15 14:30:10 -0500

    Update projects to correspond to card content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,18 @@
 import Card from "./Card"
-import square from '../assets/images/squareplaceholder.jpg'
+import thecup from '../assets/images/thecup.png'
+import reptile from '../assets/images/reptile.png'
 
 const Projects = () => {
     let key = 0
     const projects = [
-        {'title' : 'Memory Game', image : square, description : 'Test your memory with a game that shows you a list of randomized images to click.'},
-        {title : 'Sample proj', 'image' : square, 'description' : 'Sample project description blah blah blah blah lorem ipsum foo bar' }
+        {'title' : 'The Cup', image : thecup, description : 'Ever have trouble deciding on a movie to watch? The Cup is a movie roulette website. Users can select from categories like genre, streaming service, and popularity. The Cup will use TMDB\'s API to display a random movie matching the user\'s selected parameters. Built with React.', link : 'https://thecup.netlify.app/'},
+        {title : 'Remember the Reptile', 'image' : reptile, link: 'https://remember-the-reptile.netlify.app/', 'description' : 'A simple memory game. Click on any image and the cards will be shuffled around. Don\'t click the same image twice. Built with React.' }
     
     ]
 
 
     return(
-            <div className='projects'>
+            <div id='projects' className='projects'>
                 <h1 className='projects-header'>Projects</h1>
                 <div className='projects-container'>
                     <div className='cards-container'>
